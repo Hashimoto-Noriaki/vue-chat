c07329c847f8caaad80e7640352511c26bcd50de
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-09-06 14:57:42 +0900

    サインアップメソッドを追加して「登録する」ボタンを押しても画面がリロードされなり、登録されているのをコンソールを確認

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <h2>アカウント登録</h2>
-    </div>
-    <form>
+    <form @submit.prevent="signUp">
         <input type="text" required placeholder="名前" v-model="name">
         <input type="email" required placeholder="メールアドレス" v-model="email">
         <input type="password" required placeholder="パスワード" v-model="password">
         <input type="password" required placeholder="パスワード(確認用)" v-model="passwordConfirmation">
         <button>登録する</button>
     </form>
+    </div>
 </template>
 
 <script>
@@ -19,6 +19,13 @@ export default {
             email: "",
             password: "",
             passwordConfirmation: ''
+        }
+    },
+
+    methods: {
+        async signUp(){
+            console.log(this.name, this.email, this.password, this.passwordConfirmation )
+            // console.log(this.name, this.email, this.password, this.passwordConfirmation)
         }
     }
 }
